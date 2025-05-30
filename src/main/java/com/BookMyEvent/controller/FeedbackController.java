1f7d4748a7f31ce83b259e8915f5b7f7787f3348
Lyubomyr <bobboby403@gmail.com>
2025-05-30 11:21:25 +0300

    modify feedback controler

--- src/main/java/com/BookMyEvent/controller/FeedbackController.java
+++ src/main/java/com/BookMyEvent/controller/FeedbackController.java
@@ -3,14 +3,18 @@ package com.BookMyEvent.controller;
 import com.BookMyEvent.entity.UserFeedback;
 import com.BookMyEvent.entity.dto.AppResponse;
 import com.BookMyEvent.service.FeedbackService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/feedbacks")
+@RestController
+@Tag(name = "Feedback Endpoints")
+@RequestMapping("/feedbacks")
 @RequiredArgsConstructor
 @Slf4j
 public class FeedbackController {
